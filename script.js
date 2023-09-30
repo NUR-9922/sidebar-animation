c256a82b4e7396517c3209061b880e0d07b8c6da
NUR ALAM MONDAL <145200844+NUSH-9922@users.noreply.github.com>
2023-09-30 20:26:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function  ani1(){
+    document.getElementById("animation-eliment").style.animationName = "animation1";
+    document.getElementById("animation-eliment").style.animationDuration = "3s";
+}
+function  ani2(){
+    document.getElementById("animation-eliment").style.animationName = "ani2";
+    document.getElementById("animation-eliment").style.animationDuration = "4s";
+}
+function  ani3(){
+    document.getElementById("animation-eliment").style.animationName = "ani3";
+    document.getElementById("animation-eliment").style.animationDuration = "15s";
+}
+
+function  ani4(){
+    document.getElementById("animation-eliment").style.animationName = "ani4";
+    document.getElementById("animation-eliment").style.animationDuration = "10s";
+}
+
+function  ani5(){
+    document.getElementById("animation-eliment").style.animationName = "ani5";
+    document.getElementById("animation-eliment").style.animationDuration = "10s";
+}
+function  ani6(){
+    document.getElementById("animation-eliment").style.animationName = "ani6";
+    document.getElementById("animation-eliment").style.animationDuration = "10s";
+}
+function  ani7(){
+    document.getElementById("animation-eliment").style.animationName = "ani7";
+    document.getElementById("animation-eliment").style.animationDuration = ".1s";
+    document.getElementById("animation-eliment").style.animationIterationCount = "4";
+}
+function  ani8(){
+    document.getElementById("animation-eliment").style.width = "80px";
+    document.getElementById("animation-eliment").style.height = "80px";
+    document.getElementById("animation-eliment").style.borderRadius= "100%";
+    document.getElementById("animation-eliment").style.boxShadow = " 15px 15px 0 0 red";
+    document.getElementById("animation-eliment").style.backgroundColor = "transparent";
+}
+function  ani9(){
+    document.getElementById("animation-eliment").style.animationName = "ani9";
+    document.getElementById("animation-eliment").style.animationDuration = "10s";
+    document.getElementById("animation-eliment").style.animationIterationCount = "infinite";
+}
